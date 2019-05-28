5b8ab53f2c639e0247c3e90bf11319b1f1b1c71b
nullldata <42836083+nullldata@users.noreply.github.com>
2019-05-28 23:10:16 +0100

    Update PhishingFormFucker.ps1

--- PhishingFormFucker.ps1
+++ PhishingFormFucker.ps1
@@ -1,7 +1,7 @@
-$Names = @("olivia", "amelia", "isla", "emily", "ava", "lily", "mia", "sophia", "isabella", "grace", "poppy", "ella", "evie", "charlotte", "jessica", "daisy", "sophie", "freya", "alice", "sienna", "", "ivy", "harper", "ruby", "isabelle", "willow", "phoebe", "evelyn", "scarlett", "chloe", "florence", "elsie", "millie", "layla", "matilda", "rosie", "esme", "eva", "lucy", "aria", "ellie", "sofia", "maisie", "erin", "lola", "lilly", "thea", "imogen", "eliza", "bella", "molly", "hannah", "emma", "violet", "luna", "amber", "lottie", "darcie", "maya", "georgia", "elizabeth", "zara", "penelope", "holly", "nancy", "rose", "emilia", "harriet", "gracie", "darcy", "mila", "orla", "abigail", "jasmine", "eleanor", "anna", "summer", "robyn", "lexi", "heidi", "annabelle", "maria", "aurora", "leah", "darcey", "victoria", "hallie", "martha", "amelie", "katie", "bonnie", "arabella", "lacey", "annie", "niamh", "lyla", "iris", "zoe", "clara", "maddison", "megan")
-$Suffixes = 1952..2004
-$PasswordLength = 5..9
-$Domains = @("@gmail.com", "@yahoo.com", "@outlook.com", "@live.com", "@protonmail.com")
+$Names = (Invoke-WebRequest "https://raw.githubusercontent.com/powerlanguage/word-lists/master/word-list-raw.txt" | select -ExpandProperty Content | Out-String).Split("`n")
+$Suffixes = 00..2004
+$PasswordLength = 5..12
+$Domains = @("@gmail.com", "@yahoo.com", "@outlook.com", "@live.com", "@protonmail.com", "@gmail.es", "@gmail.co.uk", "@gmail.gov.uk")
 
 while($true)
 {
