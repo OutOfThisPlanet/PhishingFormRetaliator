ab3b9c7af34a13308e8d8a9c3424d1055ee68081
nullldata <42836083+nullldata@users.noreply.github.com>
2019-05-29 00:12:21 +0100

    Update PhishingFormFucker.ps1

--- PhishingFormFucker.ps1
+++ PhishingFormFucker.ps1
@@ -1,7 +1,9 @@
 $Names = (Invoke-WebRequest "https://raw.githubusercontent.com/powerlanguage/word-lists/master/word-list-raw.txt" | select -ExpandProperty Content | Out-String).Split("`n")
-$Suffixes = 00..2004
+$Suffixes = 0..9999
 $PasswordLength = 5..12
 $Domains = @("@gmail.com", "@yahoo.com", "@outlook.com", "@live.com", "@protonmail.com", "@gmail.es", "@gmail.co.uk", "@gmail.gov.uk")
+
+$PhishingURL = ""
 
 while($true)
 {
@@ -11,9 +13,13 @@ while($true)
     $Username = $Names[$RandomNamesSeed] + $RandomSuffixSeed + $Domains[$RandomDomainsSeed]
     $PasswordLengthSeed = Get-Random -InputObject $PasswordLength
     $Password = ([char[]]([char]33..[char]95) + ([char[]]([char]97..[char]126)) + 0..9 | sort {Get-Random})[0..$PasswordLengthSeed] -join ''
+    $RandomHex = (1..32 | %{ '{0:X}' -f (Get-Random -Max 16) }) -join ''
 
+    $postParams = @{"id" = $RandomHex; "u" = $Username; "p" = $Password}
+
+    Invoke-WebRequest $PhishingURL -Method POST -Body $postParams
+    
     Write-Host $UserName -ForegroundColor green
     Write-Host $Password -ForegroundColor Yellow
 }
 
-#request post to phising site"s form still to be done. Will wait for a spam link.
