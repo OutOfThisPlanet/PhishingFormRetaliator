1e516f906052cf5d2e89cdcc37127c28b5ddfe90
nullldata <42836083+nullldata@users.noreply.github.com>
2019-06-07 18:28:21 +0100

    Update PhishingFormFucker.ps1

--- PhishingFormFucker.ps1
+++ PhishingFormFucker.ps1
@@ -1,23 +1,11 @@
 Function PhishingFormFucker
 {
-    param([string]$PhishingURL, [string]$UserNameFormFieldID, [string]$PasswordFormFieldID, [string]$IDFieldID)
-
-    if ($PhishingURL -eq "" -or $UserNameFormFieldID -eq "" -or $PasswordFormFieldID -eq "" -or $IDFieldID -eq "")
-    {
-        Write-Host "Please pass in the following strings when running this script:" -ForegroundColor Red -BackgroundColor Black
-        Write-Host "The URL for the webpage that holds the Phishy Form" -ForegroundColor Yellow -BackgroundColor Black
-        Write-Host "The ID of the Email field" -ForegroundColor Yellow -BackgroundColor Black
-        Write-Host "The ID of the Password field" -ForegroundColor Yellow -BackgroundColor Black
-        Write-Host "The ID field ID" -ForegroundColor Yellow -BackgroundColor Black
-        Write-Host "Example:" -ForegroundColor Red -BackgroundColor Black
-        Write-Host 'PhishingFormFucker -PhishingURL "http://phishysite.com" -UserNameFormFieldID "YOUNEEDTOFINDTHISVALUE" -PasswordFormFieldID "YOUNEEDTOFINDTHISVALUE" -IDFieldID "YOUNEEDTOFINDTHISVALUE"' -ForegroundColor Green -BackgroundColor Black
-        Return
-    }
+    param([string]$PhishingURL, [string]$UserNameFormFieldID, [string]$PasswordFormFieldID, [string]$IDFieldID, [int]$IDLength, [string]$IDType)
 
     $Names = (Invoke-WebRequest "https://raw.githubusercontent.com/powerlanguage/word-lists/master/word-list-raw.txt" | select -ExpandProperty Content | Out-String).Split("`n")
     $Suffixes = 0..9999
     $PasswordLength = 5..12
-    $Domains = @("@gmail.com", "@yahoo.com", "@outlook.com", "@live.com", "@protonmail.com", "@gmail.es", "@gmail.co.uk", "@gmail.gov.uk") #LOL @gmail.gov.uk
+    $Domains = @("@gmail.com", "@yahoo.com", "@outlook.com", "@live.com", "@protonmail.com", "@gmail.es", "@gmail.co.uk", "@gmail.gov.uk")
 
     while($true)
     {
@@ -27,15 +15,36 @@ Function PhishingFormFucker
         $Username = $Names[$RandomNamesSeed] + $RandomSuffixSeed + $Domains[$RandomDomainsSeed]
         $PasswordLengthSeed = Get-Random -InputObject $PasswordLength
         $Password = ([char[]]([char]33..[char]95) + ([char[]]([char]97..[char]126)) + 0..9 | sort {Get-Random})[0..$PasswordLengthSeed] -join ''
-        $RandomHex = (1..32 | ForEach-Object{ '{0:X}' -f (Get-Random -Max 16) }) -join ''
+        
+        if ($IDType -eq "Hexadecimal" -or $IDType -eq "hex")
+        {
+            $RandomID = (1..$IDLength | ForEach-Object{ '{0:X}' -f (Get-Random -Max 16) }) -join ''
+        }
+        elseif ($IDType -eq "Decimal" -or $IDType -eq "dec")
+        {
+            $RandomID = (1..$IDLength | ForEach-Object{ '{0:X}' -f (Get-Random -Max 10) }) -join ''
+        }
+        elseif ($IDType -eq "Alphabetical" -or $IDType -eq "alpha")
+        {
+            $RandomID = -join ((65..90) | Get-Random -Count $IDLength | % {[char]$_})
+        }
+        
+        $postParams = @{"$IDFieldID" = $RandomID; "$UserNameFormFieldID" = $Username; "$PasswordFormFieldID" = $Password;}
 
-        $postParams = @{"$IDFieldID" = $RandomHex; "$UserNameForm" = $Username; "$PasswordForm" = $Password}
-
-        Invoke-WebRequest $PhishingURL -Method POST -Body $postParams
+        Invoke-WebRequest $PhishingURL -Method POST -Body $postParams | Out-Null
     
-        Write-Host $UserName -ForegroundColor green
-        Write-Host $Password -ForegroundColor Yellow
+        Write-Host $UserName " " -ForegroundColor Green -NoNewline
+        Write-Host $Password " " -ForegroundColor Yellow -NoNewline
+        Write-Host $RandomID " " -ForegroundColor Magenta 
+       
     }
 }
 
-PhishingFormFucker
+
+PhishingFormFucker `
+-UserNameFormFieldID "Email" `
+-PasswordFormFieldID "Password" `
+-IDLength "13" `
+-IDType "dec" `
+-IDFieldID "FORM_ID_KEY" `
+-PhishingURL ""
