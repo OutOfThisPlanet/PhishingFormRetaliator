c715aa0c775afe03eccdd49c08869a131bbed83c
nullldata <42836083+nullldata@users.noreply.github.com>
2019-06-17 17:23:00 +0100

    Update PhishingFormFucker.ps1

--- PhishingFormFucker.ps1
+++ PhishingFormFucker.ps1
@@ -43,7 +43,7 @@ Function PhishingFormFucker
 PhishingFormFucker `
 -UserNameFormFieldID "INSERT-USERNAME-FIELD-ID" ` # This is the Username field ID
 -PasswordFormFieldID "INSERT-PASSWORD-FIELD-ID" ` # This is the Password field ID
--IDType "Decimal" ` # This is to generate an formID of a specific type (Decimal, Hexidecimal, or Alphabetical)
+-IDType "Decimal" ` # This is to generate a random form ID of a specific type (Decimal, Hexidecimal, or Alphabetical)
 -IDLength "8" ` # This is for the length of the random form ID
 -IDFieldID "INSERT-FORM-ID" ` # This is the name of the form's ID
 -PhishingURL "INSERT-PHISHING-URL" # This is the phishing page's url (usually a PHP file)
